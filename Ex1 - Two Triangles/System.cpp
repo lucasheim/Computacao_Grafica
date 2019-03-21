7cba9842958f276e768c66a4b5858726910df759
Lucas Heim <lucas.heim@sap.com>
2019-03-20 21:34:35 -0300

    Initial commit

---
+++ Ex1 - Two Triangles/System.cpp
@@ -0,0 +1,170 @@
+#include "System.h"
+
+
+
+System::System()
+{
+}
+
+
+System::~System()
+{
+}
+
+int System::GLFWInit()
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+	glfwWindowHint(GLFW_SAMPLES, 4);
+
+	window = glfwCreateWindow(WIDTH, HEIGHT, "", nullptr, nullptr);
+
+	glfwGetFramebufferSize(window, &screenWidth, &screenHeight);
+
+	if (window == nullptr) {
+		std::cout << "Failed to create GLFW Window" << std::endl;
+		glfwTerminate();
+
+		return EXIT_FAILURE;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	glewExperimental = GL_TRUE;
+
+	if (glewInit() != GLEW_OK) {
+		std::cout << "Failed no init GLEW." << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	glViewport(0, 0, screenWidth, screenHeight);
+
+	return EXIT_SUCCESS;
+}
+
+int System::OpenGLSetup()
+{
+	glEnable(GL_BLEND);	// Enables blending ( glBlendFunc )
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+	glEnable(GL_DEPTH_TEST);
+
+	glEnable(GL_CULL_FACE);
+	glCullFace(GL_BACK);
+	glFrontFace(GL_CW);
+
+	return EXIT_SUCCESS;
+}
+
+int System::SystemSetup()
+{
+	coreShader = Shader("Shaders/Core/core.vert", "Shaders/Core/core.frag");
+	coreShader.Use();
+
+	return EXIT_SUCCESS;
+}
+
+void System::Run()
+{
+	coreShader.Use();
+
+	GLfloat vertices[] =
+	{
+		// Positions         // Textures
+		 0.1f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f, // Top			 (RED)
+		 0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f, // Bottom middle (GREEN)
+		-0.8f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f, // Bottom left	 (BLUE)
+		 0.1f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f, // Top			 (RED)
+		 0.7f,  0.0f, 0.0f,   0.0f, 0.0f, 1.0f, // Bottom right  (BLUE)
+		 0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f, // Bottom middle (GREEN)
+	};
+
+	GLuint VBO, VAO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+
+	// Bind the Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointer(s).
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	// Position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid *)0);
+	glEnableVertexAttribArray(0);
+
+	// Color attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid *)(3 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(1);
+
+	glBindVertexArray(0); // Unbind VAO
+
+	while (!glfwWindowShouldClose(window)) {
+		this->ShowFPS(window);
+		glfwPollEvents();
+
+#pragma region Input Handling
+
+		if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+			glfwSetWindowShouldClose(window, GLFW_TRUE);
+		}
+
+#pragma endregion
+
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		coreShader.Use();
+
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+		glBindVertexArray(0);
+
+
+		glfwSwapBuffers(window);
+	}
+
+
+}
+
+void System::ShowFPS(GLFWwindow* window)
+{
+	static double previousSeconds = 0.0;
+	static int frameCount = 0;
+	double elapsedSeconds;
+	double currentSeconds = glfwGetTime(); // returns number of seconds since GLFW started, as double float
+
+	elapsedSeconds = currentSeconds - previousSeconds;
+
+	// Limit text updates to 4 times per second
+	if (elapsedSeconds > 0.25)
+	{
+		previousSeconds = currentSeconds;
+		double fps = (double)frameCount / elapsedSeconds;
+		double msPerFrame = 1000.0 / fps;
+
+		// The C++ way of setting the window title
+		std::ostringstream outs;
+		outs.precision(3);	// decimal places
+		outs << std::fixed
+			<< "Ex. 1" << "    "
+			<< "FPS: " << fps << "    ";
+		glfwSetWindowTitle(window, outs.str().c_str());
+
+		// Reset for next average.
+		frameCount = 0;
+	}
+
+	frameCount++;
+}
+
+void System::Finish()
+{
+	coreShader.Delete();
+
+	glfwTerminate();
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# 2 triângulos
+
+Implementar com shaders, VBO, VAO e com o contador de FPS no título da janela, o desenho de 2
+triângulos coloridos com interpolação de cores.
